3ceb16e3474fdb5280ff2dac7f4a73ee15160bd2
kkatQC <polushkinaekaterina@yahoo.fr>
2022-11-03 13:29:14 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,10 +27,10 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 
 try:
-fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice 
-    streamlit.error('Please select a fruit')
-    else
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice
+  streamlit.error('Please select a fruit')
+  else
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
       # streamlit.text(fruityvice_response.json())
       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
